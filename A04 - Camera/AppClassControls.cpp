1aba0012b060d2692254b4e7e84f38a27cf6c9b8
alexandertoy <alexander.toy@hotmail.com>
2018-03-21 18:42:18 -0400

    A04 - WASD working
    It's in the title, y'all.

--- A04 - Camera/AppClassControls.cpp
+++ A04 - Camera/AppClassControls.cpp
@@ -78,9 +78,10 @@ void Application::ProcessKeyPressed(sf::Event a_event)
 		m_pCamera->SetTarget(m_pCamera->GetTarget() + vector3(0.0f, 0.0f, -1.0f));
 		break;
 	case sf::Keyboard::A:
-		std::cout << "A Pressed." << std::endl;
+		//std::cout << "A Pressed." << std::endl;
 		m_pCamera->SetPosition(m_pCamera->GetPosition() + vector3(-0.3f, 0.0f, 0.0f));
-		m_pCamera->SetUp(m_pCamera->GetUp() + vector3(0.0f, 0.3f, 0.0f));
+		m_pCamera->SetTarget(m_pCamera->GetTarget() + vector3(-0.3f, 0.0f, 0.0f));
+		//m_pCamera->SetUp(m_pCamera->GetUp() + vector3(0.0f, 0.3f, 0.0f));
 		break;
 	case sf::Keyboard::S:
 		//std::cout << "S Pressed." << std::endl;
@@ -88,7 +89,9 @@ void Application::ProcessKeyPressed(sf::Event a_event)
 		m_pCamera->SetTarget(m_pCamera->GetTarget() + vector3(0.0f, 0.0f, -1.0f));
 		break;
 	case sf::Keyboard::D:
-		std::cout << "D Pressed." << std::endl;
+		//std::cout << "D Pressed." << std::endl;
+		m_pCamera->SetPosition(m_pCamera->GetPosition() + vector3(0.3f, 0.0f, 0.0f));
+		m_pCamera->SetTarget(m_pCamera->GetTarget() + vector3(0.3f, 0.0f, 0.0f));
 		break;
 	}
 	//gui
